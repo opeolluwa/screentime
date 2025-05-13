7e7a614d874e4409bbaff202f24049b8978f4696
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-14 00:37:04 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -4,37 +4,44 @@
     class="w-full h-screen bg-sc-orange/80 bg-linear-to-r from-sc-orange to-sc-orange/80 bg-[url(assets/bg-4.jpg)] bg-center-center bg-blend-multiply bg-cover bg-no-repeat relative flex flex-col items-center justify-center px-8"
   >
     <h2 class="font-medium text-3xl mb-4 hidden">active</h2>
-    <Countdown
-      :showDays="false"
-      class="font-medium text-9xl"
-      :deadlineDate="new Date(date)"
-      countdownSize="3.75rem"
-      labelSize="1.8rem"
-      labelColor="var(--sc-faint)"
-      @timeElapsed="updateTime()"
-    />
+    <RadialProgress :progress="progress" class="">
+      <h2 class="font-black text-7xl mb-4">
+        {{ remainingTime }}
+      </h2>
+    </RadialProgress>
 
     <div
-      class="size-12 absolute right-6 bottom-6 bg-sc-orange rounded-full p-2 cursor-pointer shadow"
+      class="size-12 absolute right-6 bottom-6 bg-sc-faint rounded-full p-2 cursor-pointer shadow"
     >
-      <Cog6ToothIcon class="size-8 text-sc-faint" />
+      <Cog6ToothIcon class="size-8 text-sc-orange" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { Countdown } from "vue3-flip-countdown";
+import { ref, computed } from "vue";
 import { Cog6ToothIcon } from "@heroicons/vue/20/solid";
-import { ref } from "vue";
-const SESSION = 6 * 1000;
-const BREAK = 5 * 60 * 1000;
-const date = ref(new Date().getTime() + SESSION);
+import { useCountdown } from "@vueuse/core";
+import RadialProgress from "./components/RadialPRogress.vue";
+// Constants
+const SESSION_DURATION = 25 * 60 * 1000; // 25 minutes in ms
+const BREAK_DURATION = 5 * 60 * 1000; // 5 minutes in ms
+const progress = ref(70);
+const remainingTime = ref(formatMilliseconds(SESSION_DURATION));
+// window.setInterval(() => {
+//   let timer = SESSION_DURATION;
+//   while (timer != 0) {
+//     remainingTime.value = formatMilliseconds(timer - 1);
+//   }
+//   timer -= 1;
+// }, 1000);
 
-function updateTime() {
-  date.value = new Date().getTime() + BREAK;
+function formatMilliseconds(ms: number): string {
+  const totalSeconds = Math.floor(ms / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
 }
-
-
-
 </script>
+
 <style scoped></style>
