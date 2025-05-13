ca1a6d2284f4877cc744901e5cb8252b6bd8f2f2
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-13 19:22:36 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -11,39 +11,30 @@
       countdownSize="3.75rem"
       labelSize="1.8rem"
       labelColor="var(--sc-faint)"
+      @timeElapsed="updateTime()"
     />
 
-    <div class="size-12 absolute right-6 bottom-6 bg-sc-orange rounded-full p-2 cursor-pointer shadow">
+    <div
+      class="size-12 absolute right-6 bottom-6 bg-sc-orange rounded-full p-2 cursor-pointer shadow"
+    >
       <Cog6ToothIcon class="size-8 text-sc-faint" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {
-  isPermissionGranted,
-  requestPermission,
-  sendNotification,
-} from "@tauri-apps/plugin-notification";
-import { onMounted, ref } from "vue";
 import { Countdown } from "vue3-flip-countdown";
 import { Cog6ToothIcon } from "@heroicons/vue/20/solid";
+import { ref } from "vue";
+const SESSION = 6 * 1000;
+const BREAK = 5 * 60 * 1000;
+const date = ref(new Date().getTime() + SESSION);
 
-const percentage = ref(5);
+function updateTime() {
+  date.value = new Date().getTime() + BREAK;
+}
 
-const date = ref(new Date().getTime() + 25 * 60 * 1000);
-onMounted(async () => {
-  window.setInterval(() => {
-    percentage.value += 0.1;
-  }, 1000);
-  let permissionGranted = await isPermissionGranted();
-  if (!permissionGranted) {
-    const permission = await requestPermission();
-    permissionGranted = permission === "granted";
-  }
-  if (permissionGranted) {
-    sendNotification({ title: "Tauri", body: "Tauri is awesome!" });
-  }
-});
+
+
 </script>
 <style scoped></style>
