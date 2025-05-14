14c1d62d5e365c46f2205f4e5bd2820e4aa57f46
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-14 02:23:50 +0100

    impl sound notification

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div
     id="container"
-    class="w-full h-screen bg-sc-orange/80 bg-linear-to-r from-sc-orange to-sc-orange/80 bg-[url(assets/bg-4.jpg)] bg-center-center bg-blend-multiply bg-cover bg-no-repeat relative flex flex-col items-center justify-center px-8"
+    class="w-full h-screen bg-sc-orange/80 bg-linear-to-r from-sc-orange to-sc-orange/80 bg-[url(assets/bg-4.jpg)] bg-center bg-blend-multiply bg-cover bg-no-repeat relative flex flex-col items-center justify-center px-8"
   >
-    <h2 class="font-medium text-3xl mb-4 hidden">active</h2>
-    <RadialProgress :progress="progress" class="">
-      <h2 class="font-black text-7xl mb-4">
-        {{ remainingTime }}
-      </h2>
-    </RadialProgress>
-
+    <FlipCountdown
+      class="font-medium text-9xl"
+      :showDays="false"
+      :deadlineDate="deadline"
+      countdownSize="3.75rem"
+      labelSize="1.8rem"
+      @timeElapsed="onCountdownEnd"
+    />
     <div
-      class="size-12 absolute right-6 bottom-6 bg-sc-faint rounded-full p-2 cursor-pointer shadow"
+      class="size-12 absolute right-6 bottom-6 bg-sc-dark rounded-full p-2 cursor-pointer shadow"
+
     >
       <Cog6ToothIcon class="size-8 text-sc-orange" />
     </div>
@@ -19,29 +21,44 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import { Cog6ToothIcon } from "@heroicons/vue/20/solid";
-import { useCountdown } from "@vueuse/core";
-import RadialProgress from "./components/RadialPRogress.vue";
-// Constants
-const SESSION_DURATION = 25 * 60 * 1000; // 25 minutes in ms
-const BREAK_DURATION = 5 * 60 * 1000; // 5 minutes in ms
-const progress = ref(70);
-const remainingTime = ref(formatMilliseconds(SESSION_DURATION));
-// window.setInterval(() => {
-//   let timer = SESSION_DURATION;
-//   while (timer != 0) {
-//     remainingTime.value = formatMilliseconds(timer - 1);
-//   }
-//   timer -= 1;
-// }, 1000);
+import { Countdown as FlipCountdown } from "vue3-flip-countdown";
+import { useSound } from "@vueuse/sound";
 
-function formatMilliseconds(ms: number): string {
-  const totalSeconds = Math.floor(ms / 1000);
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
-  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
-}
+import sessionOverSrc from "./assets/session-over.mp3";
+import breakOverSrc from "./assets/break-over.mp3";
+import alarmSoundSrc from "./assets/alarm.mp3";
+
+const SESSION_DURATION = 6 * 1000;
+const BREAK_DURATION = 6 * 1000;
+
+const deadline = ref(new Date(Date.now() + SESSION_DURATION));
+const isSession = ref(false);
+const isBreak = ref(true);
+
+const { play: notifySessionOver } = useSound(sessionOverSrc);
+const { play: notifyBreakOver } = useSound(breakOverSrc);
+const { play: alarm } = useSound(alarmSoundSrc);
+
+const onCountdownEnd = () => {
+  if (isSession.value) {
+    alarm();
+    notifySessionOver();
+
+    isBreak.value = true;
+    isSession.value = false;
+
+    window.setTimeout(() => {
+      deadline.value = new Date(Date.now() + BREAK_DURATION);
+    }, BREAK_DURATION);
+  } else if (isBreak.value) {
+    alarm();
+    notifyBreakOver();
+
+    isBreak.value = false;
+    isSession.value = true;
+    deadline.value = new Date(Date.now() + SESSION_DURATION);
+  }
+};
 </script>
-
-<style scoped></style>
