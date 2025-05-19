c96759731a9b473d58c13ae6d9fb9825477a6af4
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-19 10:35:49 +0100

    reduce windowsize and position

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,24 @@
     class="w-full h-screen bg-sc-orange/80 bg-linear-to-r from-sc-orange to-sc-orange/80 bg-[url(assets/bg-4.jpg)] bg-center bg-blend-multiply bg-cover bg-no-repeat relative flex flex-col items-center justify-center px-8"
   >
     <FlipCountdown
-      class="font-medium text-9xl"
+      class="font-medium text-3xl"
       :showDays="false"
       :deadlineDate="deadline"
-      countdownSize="3.75rem"
-      labelSize="1.8rem"
+      countdownSize="2rem"
+      labelSize="16px"
       @timeElapsed="onCountdownEnd"
     />
+
     <div
-      class="size-12 absolute right-6 bottom-6 bg-sc-dark rounded-full p-2 cursor-pointer shadow"
+      class="size-8 absolute right-4 bottom-4 bg-sc-dark rounded-full p-2 cursor-pointer shadow flex items-center justify-center"
     >
-      <Cog6ToothIcon class="size-8 text-sc-orange" />
+      <Cog6ToothIcon class="size-6 text-sc-orange" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { Cog6ToothIcon } from "@heroicons/vue/20/solid";
 import { Countdown as FlipCountdown } from "vue3-flip-countdown";
 import { useSound } from "@vueuse/sound";
@@ -41,6 +42,7 @@ const { play: notifyBreakOver } = useSound(breakOverSrc);
 const { play: alarm } = useSound(alarmSoundSrc);
 
 const onCountdownEnd = () => {
+  // while (!isBreak.value && isSession.value) {
   if (isSession.value) {
     alarm();
     notifySessionOver();
@@ -48,16 +50,14 @@ const onCountdownEnd = () => {
     isBreak.value = true;
     isSession.value = false;
 
-    window.setTimeout(() => {
+    window.setTimeout(function () {
+      isBreak.value = false;
+      isSession.value = true;
       deadline.value = new Date(Date.now() + BREAK_DURATION);
+      alarm();
+      notifyBreakOver();
     }, BREAK_DURATION);
-  } else if (isBreak.value) {
-    alarm();
-    notifyBreakOver();
-
-    isBreak.value = false;
-    isSession.value = true;
-    deadline.value = new Date(Date.now() + SESSION_DURATION);
   }
+  // }
 };
 </script>
